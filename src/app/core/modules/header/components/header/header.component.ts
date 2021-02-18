e32f8c105fbf7ca9b63ab303d39a7aa6e060ec39
Ali Akbar Shahi <ali.akbar.shahi.junior@gmail.com>
2021-02-18 22:11:12 +0330

    header sign out fixed

--- src/app/core/modules/header/components/header/header.component.ts
+++ src/app/core/modules/header/components/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'EW-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent implements OnInit, OnChanges {
+  logged: boolean = false;
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.logged = localStorage.getItem('User') == null;
+  }
+
+  ngOnChanges() {
+    this.logged = localStorage.getItem('User') == null;
+  }
+
+  onSignOut() {
+    localStorage.clear();
+    this.router.navigate(['/register/sign-in']);
+    this.ngOnInit();
+  }
 }
--- src/app/core/modules/header/header.module.ts
+++ src/app/core/modules/header/header.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { MaterialModule } from '../material/material.module';
 
 const Components: any[] = [HeaderComponent];
 
 @NgModule({
   declarations: [...Components],
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
   exports: [...Components],
 })
 export class HeaderModule {}
